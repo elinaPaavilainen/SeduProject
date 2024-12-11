39b55241be1a9da78ca7b7013ae95c81d24f0d2d
Elina Paavilainen <elina.m.paavilainen@gmail.com>
2024-12-11 12:54:01 +0200

    Updated CreateUser.xaml

--- Varastokirjanpito_projekti/Varastokirjanpito_projekti/Pages/CreateUser.xaml.cs
+++ Varastokirjanpito_projekti/Varastokirjanpito_projekti/Pages/CreateUser.xaml.cs
@@ -6,13 +6,11 @@ namespace Varastokirjanpito_projekti.Pages;
 public partial class CreateUser : ContentPage
 {
     private readonly ApiService _apiService;
-    public ObservableCollection<Users> UsersList { get; set; }
     public CreateUser()
     {
         InitializeComponent();
         _apiService = new ApiService();
-        UsersList = new ObservableCollection<Users>();
-        UsersListView.ItemsSource = UsersList;
+
     }
     private async void CreateNewUser(object sender, EventArgs e)
     {
@@ -35,37 +33,5 @@ public partial class CreateUser : ContentPage
         {
             await DisplayAlert("Error", $"Failed to save data: {ex.Message}", "OK"); 
         }
-    }
-    private async void SearchUserClicked(object sender, EventArgs e)
-    {
-        try
-        {
-            string UserName = SearchUser.Text;
-            IEnumerable<Users> result;
-
-            if (string.IsNullOrEmpty(UserName))
-            {
-                result = await _apiService.GetUsersControllerDataAsync();
-            }
-            else
-            {
-                result = await _apiService.SearchUsersAsync(UserName);
-            }
-            UsersList.Clear();
-            foreach (var user in result)
-            {
-                UsersList.Add(user);
-            }
-            InfoLabel.Text = "Klikkaa valitsemaasi k‰ytt‰j‰‰ n‰hd‰ksesi kaikki tiedot.";
-        }
-        catch
-        (Exception ex)
-        {
-            InfoLabel.Text = $"Error: {ex.Message}";
-        }
-    }
-    private async void OnUserTapped(object sender, EventArgs e)
-    {
-        InfoLabel.Text = "Clicked";
     }
 }
--- Varastokirjanpito_projekti/Varastokirjanpito_projekti/Pages/DeleteUser.xaml.cs
+++ Varastokirjanpito_projekti/Varastokirjanpito_projekti/Pages/DeleteUser.xaml.cs
@@ -1,9 +1,49 @@
+using System.Collections.ObjectModel;
+using SharedModels;
+
 namespace Varastokirjanpito_projekti.Pages;
 
 public partial class DeleteUser : ContentPage
 {
-	public DeleteUser()
+    private readonly ApiService _apiService;
+    public ObservableCollection<Users> UsersList { get; set; }
+    public DeleteUser()
 	{
 		InitializeComponent();
-	}
+        _apiService = new ApiService();
+        UsersList = new ObservableCollection<Users>();
+        UsersListView.ItemsSource = UsersList;
+    }
+    private async void SearchUserClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            string UserName = SearchUser.Text;
+            IEnumerable<Users> result;
+
+            if (string.IsNullOrEmpty(UserName))
+            {
+                result = await _apiService.GetUsersControllerDataAsync();
+            }
+            else
+            {
+                result = await _apiService.SearchUsersAsync(UserName);
+            }
+            UsersList.Clear();
+            foreach (var user in result)
+            {
+                UsersList.Add(user);
+            }
+            InfoLabel.Text = "Klikkaa valitsemaasi k‰ytt‰j‰‰ n‰hd‰ksesi kaikki tiedot.";
+        }
+        catch
+        (Exception ex)
+        {
+            InfoLabel.Text = $"Error: {ex.Message}";
+        }
+    }
+    private async void OnUserTapped(object sender, EventArgs e)
+    {
+        InfoLabel.Text = "Clicked";
+    }
 }
